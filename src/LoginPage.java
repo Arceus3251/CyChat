fd08012fbba88d8e1cd87a10c7c2f80a432c1461
Arceus3251 <arceus3251@gmail.com>
2022-10-12 10:24:32 -0500

    Added Option to create new user (Doesn't do anything yet.), Also added comments to make lives easier for checking incorrect attempts.
    Signed-off-by: Arceus3251 <arceus3251@gmail.com>

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -7,14 +7,18 @@ public class LoginPage {
     public LoginPage() {
         JFrame frame = new JFrame();
         frame.setSize(500, 200);
-        GridLayout labelLayout = new GridLayout(2, 1);
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("CyChat Login");
+        GridLayout labelLayout = new GridLayout(3, 1);
         //Creating Text for side
         JLabel userLabel = new JLabel("Username:");
         JLabel passwordLabel = new JLabel("Password:");
+        JLabel retypeLabel = new JLabel("Retype Password:");
         JPanel labelPanel = new JPanel();
         labelPanel.setLayout(labelLayout);
         labelPanel.add(userLabel);
         labelPanel.add(passwordLabel);
+        JPasswordField retypePass = new JPasswordField();
         //Creating Fields for side
         JTextField userField = new JTextField();
         JPasswordField passField = new JPasswordField();
@@ -29,6 +33,37 @@ public class LoginPage {
             information[1] = Arrays.toString(passField.getPassword());
             frame.dispose();
         });
+        //Creating the Signup Button
+        JButton signUpButton = new JButton("Sign Up");
+        signUpButton.addActionListener(e ->{
+            if(!((Arrays.toString(passField.getPassword())).equals(Arrays.toString(retypePass.getPassword())))){
+                JOptionPane.showMessageDialog(null, "Passwords do not match. Please Try again.");
+            }
+            else {
+                String userName = userField.getText();
+                String pass = Arrays.toString(passField.getPassword());
+                //Do something with it.. Sign up.. Probably, kek.
+                JOptionPane.showMessageDialog(null, "User Created! Please log in.");
+                userField.setText("");
+                labelPanel.remove(2);
+                fieldPanel.remove(2);
+                frame.remove(signUpButton);
+                frame.add(BorderLayout.SOUTH, submitButton);
+                labelPanel.updateUI();
+            }
+        });
+        //Adding New User functionality
+        JCheckBox newUserBox = new JCheckBox();
+        newUserBox.setText("New User?");
+        newUserBox.addActionListener(e->{
+            labelPanel.remove(2);
+            labelPanel.add(retypeLabel);
+            fieldPanel.add(retypePass);
+            labelPanel.updateUI();
+            frame.remove(submitButton);
+            frame.add(BorderLayout.SOUTH, signUpButton);
+        });
+        labelPanel.add(newUserBox);
         //Begin adding objects to the frame
         frame.add(fieldPanel);
         frame.add(BorderLayout.WEST, labelPanel);
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 public class Main {
     public static void main(String[] args){
         //This is disgusting, but hey it works.
-        LoginPage loginPage = new LoginPage();
-        String[] input = loginPage.getInfo();
-        while(input[0] == null){
-            input = loginPage.getInfo();
-        }
-        //input should now be an array containing User Name as a String and Password as a Character Array. We need to authenticate this.
+        //Will Contain Signing Up for our service as well, I marked in a comment where that is in "LoginPage.java" <3
+
+        //Adding logic for retry attempts, adjust as needed.
+
+        //for(int i = 3; i>-1; i--){
+            LoginPage loginPage = new LoginPage();
+            String[] input = loginPage.getInfo();
+            while(input[0] == null){
+                input = loginPage.getInfo();
+            }
+            //input should now be an array containing User Name as a String (input[0]) and Password as a Character Array (input[1]). We need to authenticate this.
+            //if(login is valid){
+                //break;
+            //}
+            //if(i==0){
+                //JOptionPane.showMessageDialog(null, "Too many invalid attempts.");
+                //System.exit(1);
+            //}
+            //JOptionPane.showMessageDialog(null, "Invalid Username and/or Password, you have "+i+" attempts remaining");
+        //}
         //From confirming authentication, we should send a packet out to the server saying we are alive.
         //From here, open contact List?
     }
