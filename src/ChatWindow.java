4c0919869f9ff69baf116e68969c450148fd9eb0
Arceus3251 <arceus3251@gmail.com>
2022-10-19 10:18:48 -0500

    Added structure for a chat window, will add more stuff later, making things arbitrary until packets are implemented.
    Signed-off-by: Arceus3251 <arceus3251@gmail.com>

--- src/ChatWindow.java
+++ src/ChatWindow.java
@@ -1,7 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ChatWindow {
-    public void ChatWindow(String recipName){
-
+    public ChatWindow(String recipName){
+        JFrame frame = new JFrame(recipName);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        JMenuBar actionBar = new JMenuBar();
+        //TODO: Add Top Bar Actions
+        JPanel sendingPanel = new JPanel();
+        //TODO: Add elements required to send chat message textField and send button, add action listener for enter.
+        JScrollPane messagesPanel = new JScrollPane();
+        //TODO: Take message packet and throw it on here.
+        frame.add(BorderLayout.NORTH, actionBar);
+        frame.add(BorderLayout.SOUTH, sendingPanel);
+        frame.add(messagesPanel);
+        frame.setVisible(true);
     }
 }
