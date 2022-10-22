537e70d31cb59a8bd171d85d428da28f73c63d4a
Sylladex <111763457+Sylladex@users.noreply.github.com>
2022-10-21 19:08:15 -0500

    Add files via upload

--- src/Encryption.java
+++ src/Encryption.java
@@ -12,24 +12,24 @@ public class Encryption {
 
     //generates key using chosen algorithm
     public void init() throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES"); //list of algorithms that can be used:
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES"); //list of algorithms that can be used for encryption/decryption:
         keyGenerator.init(KEY_SIZE);                                 //https://docs.oracle.com/en/java/javase/11/docs/specs/security/standard-names.html#cipher-algorithm-names
-        key = keyGenerator.generateKey();
+        key = keyGenerator.generateKey();                            //the lists of algorithm modes and padding modes are also included in that link ^^^
     }
 
-    //converts string to bytes, encrypts bytes using chosen cipher
+    //converts string to bytes, encrypts bytes using the chosen algorithm, algorithm mode, and padding mode
     public String encrypt(String data) throws Exception {
         byte[] dataInBytes = data.getBytes();
-        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
-        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
+        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding"); //(Algorithm) / (Algorithm Mode) / (Padding Mode)
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);            //some algorithm modes and padding modes may not be compatible with certain algorithms, will have to test this
         byte[] encryptedBytes = encryptionCipher.doFinal(dataInBytes);
         return encode(encryptedBytes);
     }
 
-    //decrypts byes, converts bytes to string
+    //decrypts bytes, converts bytes to string
     public String decrypt(String encryptedData) throws Exception {
         byte[] dataInBytes = decode(encryptedData);
-        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding"); //(Algorithm) / (Algorithm Mode) / (Padding Mode)
         GCMParameterSpec spec = new GCMParameterSpec(DATA_LENGTH, encryptionCipher.getIV());
         decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
         byte[] decryptedBytes = decryptionCipher.doFinal(dataInBytes);
