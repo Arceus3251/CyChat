194097ebd91321c0c7cafcd802ce145d3c122012
Itzymtz0501 <igm005@shsu.edu>
2022-10-24 08:43:23 -0500

    creating GUI for chat screen.

--- src/ChatWindow.java
+++ src/ChatWindow.java
@@ -1,14 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.net.
 
-public class ChatWindow {
+public class ChatWindow extends JFrame {
     //public ChatWindow(){
         //initComponents();
-    //}
     private void intiComponents(){
-    JLabel heading=new JLabel("Chat");
-    JTextArea messageScreen= new JTextArea();
-    JTextField messageInput=new JTextField();
-    Font font=new Font("Roboto",Font.PLAIN,20);
+        JLabel heading=new JLabel("Chat");
+        JTextArea messageScreen= new JTextArea();
+        JTextField messageInput=new JTextField();
+        Font font=new Font("Roboto",Font.PLAIN,20);
+
+    }
+    public ChatWindow(){
+        try{
+            System.out.println("sending request");
+            socket =new Socket("127.0.0.1",7777 );
+            System.out.println("successful connection");
+        }
+
     }
 }
