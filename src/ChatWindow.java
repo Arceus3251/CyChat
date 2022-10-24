4dc0dce6f1944e0e204de778568a547f9d9c35dd
Itzymtz0501 <igm005@shsu.edu>
2022-10-24 08:21:23 -0500

    update

--- src/ChatWindow.java
+++ src/ChatWindow.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class ChatWindow {
+    //public ChatWindow(){
+        //initComponents();
+    //}
+    //private void intiComponents(){
+    private JLabel heading=new JLabel("Chat");
+    private JTextArea messageScreen= new JTextArea();
+    private JTextField messageInput=new JTextField();
+    private Font font=new Font("Roboto",Font.PLAIN,20;)
+    //}
 }
