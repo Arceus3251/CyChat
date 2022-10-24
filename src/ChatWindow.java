d316dac07a2e114c634c36c1219549df86acb8e1
Itzymtz0501 <igm005@shsu.edu>
2022-10-24 08:58:10 -0500

    creating GUI for chat screen.

--- src/ChatWindow.java
+++ src/ChatWindow.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.net.
 
 public class ChatWindow extends JFrame {
-    //public ChatWindow(){
+    //pu blic ChatWindow(){
         //initComponents();
     private void intiComponents(){
         JLabel heading=new JLabel("Chat");
